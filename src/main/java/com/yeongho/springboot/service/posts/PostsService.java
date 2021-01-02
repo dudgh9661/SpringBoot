65cdf88bf37f91f93256666fad3516ccb97f8250
Kimyeongho <dudgh9661@naver.com>
2021-01-02 10:47:59 +0900

    JPA Auditing 테스트 케이스 통과

--- src/main/java/com/yeongho/springboot/service/posts/PostsService.java
+++ src/main/java/com/yeongho/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yeongho.springboot.service.posts;
 
+import com.yeongho.springboot.domain.posts.Posts;
 import com.yeongho.springboot.domain.posts.PostsRepository;
+import com.yeongho.springboot.web.Dto.PostsResponseDto;
 import com.yeongho.springboot.web.Dto.PostsSaveRequestDto;
+import com.yeongho.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById( Long id ) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 
 }
